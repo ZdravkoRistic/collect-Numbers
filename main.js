c8a79f83d6611a85036f7cedb0a038d7ec8ed6ef
ZdravkoRistic <easycodemaster@gmail.com>
2023-05-13 18:22:20 +0200

    modified code created again from scratch

--- main.js
+++ main.js
@@ -1,82 +1,34 @@
-let boxHolder = document.querySelector(".box-holder");
+let container = document.querySelector(".container");
 let selectedNumbers = document.querySelector(".selected-numbers");
+let selectedNumbersArray = [];
 
-function fillGrid() {
-  let html = "";
+makeColumns();
 
-  for (i = 0; i < 100; i++) {
-    let rand = Math.floor(Math.random() * 6) + 1;
-    html += `
-    <div class="cube" data-index="${i}">${rand}</div>
-    `.trim();
-  }
-  boxHolder.innerHTML = html;
-}
 
-fillGrid();
-
-let cubes = document.querySelectorAll(".cube");
-
-lastTenCubes(cubes);
-
-function lastTenCubes(cubes) {
-  cubes.forEach((cube, index) => {
-    if (index >= 90) {
-      cube.classList.remove("cube");
-      cube.classList.add("available-cube");
-      cube.addEventListener("click", selectedCube);
-    }
-  });
-}
-
-function selectedCube() {
-  let html = "";
-  let selected = this.innerHTML;
-  this.style.visibility = "hidden";
+function makeColumns() {
+  let columns = ``;
   for (let i = 0; i < 10; i++) {
-    html = `<div class="available-cube">${selected}</div>`;
+    columns += `<div class="column">${fillColumns()}</div>`;
   }
-  selectedNumbers.innerHTML += html;
-
- 
-  nextAvailable(this);
-  
- 
+  container.innerHTML = columns;
+  let column = document.querySelectorAll(".column");
+  column.forEach((el) => el.addEventListener("click", reduceColumn));
 }
 
-function nextAvailable(current) {
-    let selectedIndex = current.getAttribute("data-index");
-    let nextAvailable = cubes[selectedIndex - 10];
-    nextAvailable.classList.remove("cube");
-    nextAvailable.classList.add("available-cube");
-    nextAvailable.addEventListener("click", selectedCube);
+
+function fillColumns() {
+  let box = ``;
+  for (let i = 0; i < 10; i++) {
+    let rand = randomNumber();
+    box += `<div class="box" data-id=${rand}>${rand}</div>`;
   }
-  
+  return box;
+}
 
 
-
-  
-  
-  
-  
-  
-  
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function randomNumber() {
+  let random = Math.ceil(Math.random() * 6);
+  return random;
+}
 
 
